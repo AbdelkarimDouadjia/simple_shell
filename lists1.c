97e53de58354308798e0494d5c46e85accb1b9b7
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-07-26 13:29:49 +0100

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -1,123 +1,118 @@
 #include "shell.h"
 
 /**
- * list_len - determines length of linked list
- * @h: pointer to first node
- *
- * Return: size of list
+ * liss_lengs - give us the size(len) of linked-list
+ * @h:  the begin node has been pointered
+ * Return: length if the linked-list
  */
-size_t list_len(const list_t *h)
+size_t liss_lengs(const list_s *h)
 {
-	size_t i = 0;
+size_t d = 0;
 
-	while (h)
-	{
-		h = h->next;
-		i++;
-	}
-	return (i);
+while (h)
+{
+	h = h->next;
+	d++;
+}
+return (d);
 }
 
 /**
- * list_to_strings - returns an array of strings of the list->str
- * @head: pointer to first node
- *
- * Return: array of strings
+ * liss_to_str - gives us  a list of multiple strings using the list->str funct.
+ * @head: the begin node has been pointered
+ * Return: multiples of strings.
  */
-char **list_to_strings(list_t *head)
+char **liss_to_str(list_s *head)
 {
-	list_t *node = head;
-	size_t i = list_len(head), j;
-	char **strs;
-	char *str;
+list_s *noe = head;
+size_t d = liss_lengs(head), w;
+char **strs;
+char *str;
 
-	if (!head || !i)
-		return (NULL);
-	strs = malloc(sizeof(char *) * (i + 1));
-	if (!strs)
-		return (NULL);
-	for (i = 0; node; node = node->next, i++)
-	{
-		str = malloc(_strlen(node->str) + 1);
-		if (!str)
-		{
-			for (j = 0; j < i; j++)
-				free(strs[j]);
-			free(strs);
-			return (NULL);
-		}
-
-		str = _strcpy(str, node->str);
-		strs[i] = str;
-	}
-	strs[i] = NULL;
-	return (strs);
-}
-
-
-/**
- * print_list - prints all elements of a list_t linked list
- * @h: pointer to first node
- *
- * Return: size of list
- */
-size_t print_list(const list_t *h)
+if (!head || !d)
+return (NULL);
+strs = malloc(sizeof(char *) * (d + 1));
+if (!strs)
+return (NULL);
+for (d = 0; noe; noe = noe->next, d++)
 {
-	size_t i = 0;
-
-	while (h)
-	{
-		_puts(convert_number(h->num, 10, 0));
-		_putchar(':');
-		_putchar(' ');
-		_puts(h->str ? h->str : "(nil)");
-		_puts("\n");
-		h = h->next;
-		i++;
-	}
-	return (i);
-}
-
-/**
- * node_starts_with - returns node whose string starts with prefix
- * @node: pointer to list head
- * @prefix: string to match
- * @c: the next character after prefix to match
- *
- * Return: match node or null
- */
-list_t *node_starts_with(list_t *node, char *prefix, char c)
+str = malloc(_stlesn(noe->str) + 1);
+if (!str)
 {
-	char *p = NULL;
-
-	while (node)
-	{
-		p = starts_with(node->str, prefix);
-		if (p && ((c == -1) || (*p == c)))
-			return (node);
-		node = node->next;
-	}
+	for (w = 0; w < d; w++)
+		free(strs[w]);
+	free(strs);
 	return (NULL);
 }
 
-/**
- * get_node_index - gets the index of a node
- * @head: pointer to list head
- * @node: pointer to the node
- *
- * Return: index of node or -1
- */
-ssize_t get_node_index(list_t *head, list_t *node)
-{
-	size_t i = 0;
+str = _stcocp(str, noe->str);
+strs[d] = str;
+}
+strs[d] = NULL;
+return (strs);
+}
 
-	while (head)
-	{
-		if (head == node)
-			return (i);
-		head = head->next;
-		i++;
-	}
-	return (-1);
+
+/**
+ ** priin_liss - returns all the nodes of a (linked-list) => list_s 
+ ** @h: the begin node element has been pointered
+ ** Return: leng of liked-list
+ **/
+size_t priin_liss(const list_s *h)
+{
+size_t d = 0;
+
+while (h)
+{
+_puutss(converr_nuum(h->num, 10, 0));
+_puuchir(':');
+_puuchir(' ');
+_puutss(h->str ? h->str : "(nil)");
+_puutss("\n");
+h = h->next;
+d++;
+}
+return (d);
+}
+
+/**
+ * start_nodd_wi - Gives an element node to whose string begin with prefix element .
+ * @noe: elemn of the begin head of linked-list
+ * @prefix: matching the begin strings.
+ * @c: will pick char after prefix characteres and will matching it.
+ * Return: nothing(null) or matching node
+ */
+list_s *start_nodd_wi(list_s *noe, char *prefix, char c)
+{
+char *p = NULL;
+
+while (noe)
+{
+p = strest_wit(noe->str, prefix);
+if (p && ((c == -1) || (*p == c)))
+	return (noe);
+noe = noe->next;
+}
+return (NULL);
+}
+
+/**
+ * gitt_index_index - give the index of the node element in linked-list .
+ * @head: the begin node has been pointered
+ * @noe: elemn of the begin head of linked-list
+ * Return: -1 always, otherwise index of the node element .
+ */
+ssize_t gitt_index_index(list_s *head, list_s *noe)
+{
+size_t d = 0;
+
+while (head)
+{
+if (head == noe)
+	return (d);
+head = head->next;
+d++;
+}
+return (-1);
 }
 
