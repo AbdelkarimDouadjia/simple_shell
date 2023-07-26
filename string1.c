3369e94053d55b2679d5d6b5620ef7f26e1c6634
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-07-26 16:27:00 +0100

    Update string1.c

--- string1.c
+++ string1.c
@@ -1,88 +1,96 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
- *
- * Return: pointer to destination
- */
-char *_strcpy(char *dest, char *src)
+ ** _stcocp - string has been copied.
+ ** @dest: the location of dest 
+ ** @src: it is a source.
+ ** Return: pointer to the dest variable.
+ **/
+char *_stcocp(char *dest, char *src)
 {
-	int i = 0;
-
-	if (dest == src || src == 0)
-		return (dest);
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
+	
+	int d = 0;
+		if (dest == src || src == 0)
 	return (dest);
+	while (src[d])
+	{
+
+		
+		dest[d] = src[d];
+	
+		d++;
+		
+		}
+dest[d] = 0;
+		return (dest);
 }
 
 /**
- * _strdup - duplicates a string
- * @str: the string to duplicate
- *
- * Return: pointer to the duplicated string
+ * _strtprp - duplicates a string / the entry string will be duplicated.
+ * @str: the string to duplicate / it is a variable to stored the duplicated string
+ * Return: the str var will be pointered
  */
-char *_strdup(const char *str)
+char *_strtprp(const char *str)
 {
-	int length = 0;
-	char *ret;
-
-	if (str == NULL)
+		int length = 0;
+	
+char *ret;
+	if (str == NULL){
 		return (NULL);
-	while (*str++)
+	}
+		while (*str++)
 		length++;
-	ret = malloc(sizeof(char) * (length + 1));
-	if (!ret)
-		return (NULL);
-	for (length++; length--;)
-		ret[length] = *--str;
-	return (ret);
+			ret = malloc(sizeof(char) * (length + 1));
+if (!ret){
+	return (NULL);
+}
+for (length++; length--;)
+	ret[length] = *--str;
+return (ret);
 }
 
 /**
- * _puts - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
- */
-void _puts(char *str)
+ ** _puutss - givees us the string has on the input field. 
+ ** @str: the string to be printed / print the entry string
+ ** Return: void
+ **/
+void _puutss(char *str)
 {
-	int i = 0;
+	int d = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
+		if (!str){
+	return;
+		}
+
+	
+	while (str[d] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
-	}
+		
+_puuchir(str[d]);
+	d++;
+}
 }
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ ** _puuchir - give the char c to stdout funct and print it
+ ** @c: it will print the given char
+ ** Return: if(1) => correct.
+ ** 	   if (-1) it will return an error.
+ **/
+int _puuchir(char c)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
+	
+static int d;
+			static char buf[WRITE_BUF_SIZE];
+if (c == BUF_FLUSH || d >= WRITE_BUF_SIZE)
+		{
+	write(1, buf, d);
+		d = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	
+if (c != BUF_FLUSH){
+	buf[d++] = c;
+}
+return (1);
 }
 
