6d8b574cde62b5e938f760386dae3be25844228f
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-07-26 19:26:49 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -92,7 +92,7 @@ typedef struct pasinfo
 	int cmd_buf_type;
 	int readfd;
 	int histcount;
-}info_s;
+} info_s;
 #define INFO_INIT\
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
