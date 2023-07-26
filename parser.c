7a15e9963ec6b13bfa25375ec12e1eff5c5fa199
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-07-26 14:19:28 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,87 +1,84 @@
 #include "shell.h"
 
 /**
- * is_cmd - determines if a file is an executable command
- * @info: the info struct
- * @path: path to the file
- *
- * Return: 1 if true, 0 otherwise
+ * is_comoodd - try to test the file to check if he execute commandes or not .
+ * @inf: a structure inf.
+ * @path: link to the directory file
+ * Return: 1 on correct, else 0 .
  */
-int is_cmd(info_t *info, char *path)
+int is_comoodd(info_s *inf, char *path)
 {
 	struct stat st;
-
-	(void)info;
+	
+	(void)inf;
 	if (!path || stat(path, &st))
-		return (0);
-
+	return (0);
+	
 	if (st.st_mode & S_IFREG)
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
 
 /**
- * dup_chars - duplicates characters
- * @pathstr: the PATH string
- * @start: starting index
- * @stop: stopping index
- *
- * Return: pointer to new buffer
+ * chaa_rdupp -  char with duplicate every one 0
+ * @pathstr: a PATH is the strings .
+ * @start: index in the beginning
+ * @stop: index has halting.
+ * Return: pointing to new bufe.
  */
-char *dup_chars(char *pathstr, int start, int stop)
+char *chaa_rdupp(char *pathstr, int start, int stop)
 {
-	static char buf[1024];
-	int i = 0, k = 0;
+static char buf[1024];
+int d = 0, w = 0;
 
-	for (k = 0, i = start; i < stop; i++)
-		if (pathstr[i] != ':')
-			buf[k++] = pathstr[i];
-	buf[k] = 0;
-	return (buf);
+for (w = 0, d = start; d < stop; d++)
+if (pathstr[d] != ':')
+	buf[w++] = pathstr[d];
+buf[w] = 0;
+return (buf);
 }
 
 /**
- * find_path - finds this cmd in the PATH string
- * @info: the info struct
- * @pathstr: the PATH string
- * @cmd: the cmd to find
- *
- * Return: full path of cmd if found or NULL
- */
-char *find_path(info_t *info, char *pathstr, char *cmd)
+ ** fedn_puht - a path of string identifier the com.
+ ** @inf: structure of data info.
+ ** @pathstr: a PATH is an string of characteres.
+ ** @cmd: a commande to serach for it
+ ** Return: a path of commande , else give us a NULL.
+ **/
+char *fedn_puht(info_s *inf, char *pathstr, char *cmd)
 {
-	int i = 0, curr_pos = 0;
-	char *path;
+int d = 0, curr_pos = 0;
+char *path;
 
-	if (!pathstr)
-		return (NULL);
-	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
-	{
-		if (is_cmd(info, cmd))
-			return (cmd);
-	}
-	while (1)
-	{
-		if (!pathstr[i] || pathstr[i] == ':')
-		{
-			path = dup_chars(pathstr, curr_pos, i);
-			if (!*path)
-				_strcat(path, cmd);
-			else
-			{
-				_strcat(path, "/");
-				_strcat(path, cmd);
-			}
-			if (is_cmd(info, path))
-				return (path);
-			if (!pathstr[i])
-				break;
-			curr_pos = i;
-		}
-		i++;
-	}
+if (!pathstr)
 	return (NULL);
+if ((_stlesn(cmd) > 2) && strest_wit(cmd, "./"))
+{
+	if (is_comoodd(inf, cmd))
+		return (cmd);
+}
+while (1)
+{
+	if (!pathstr[d] || pathstr[d] == ':')
+	{
+		path = chaa_rdupp(pathstr, curr_pos, d);
+		if (!*path)
+			_stceat(path, cmd);
+		else
+		{
+			_stceat(path, "/");
+			_stceat(path, cmd);
+		}
+		if (is_comoodd(inf, path))
+			return (path);
+		if (!pathstr[d])
+			break;
+		curr_pos = d;
+	}
+	d++;
+}
+return (NULL);
 }
 
